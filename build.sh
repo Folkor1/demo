9dffd297dd44d6669f261634f3e3278abd65284b
Folkor1 <98771910+Folkor1@users.noreply.github.com>
2024-02-13 19:12:30 +0000

    25x

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python bookingpiano/manage.py createsuperuser --no-input
+fi
