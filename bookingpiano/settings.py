5c8b0ea6c012c2c61f31088aab2010e475b3fa2d
Folkor1 <98771910+Folkor1@users.noreply.github.com>
2024-02-13 16:04:59 +0000

    2x

--- bookingpiano/settings.py
+++ bookingpiano/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["demo-tomn.onrender.com", "localhost"]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django_filters',
     'cloudinary',
     'crispy_forms',
+    'render.apps.RenderConfig',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
