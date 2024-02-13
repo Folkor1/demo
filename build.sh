3132193df105f94209b0f6bfc52eff3ba53ef37a
Folkor1 <98771910+Folkor1@users.noreply.github.com>
2024-02-13 17:30:09 +0000

    7x

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 poetry install
 
+add django
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 
 chmod a+x build.sh
-
-poetry add django
 
 poetry add gunicorn
