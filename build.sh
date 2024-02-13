edfe331aa682da15f0d878b28240167a1f32ddce
Folkor1 <98771910+Folkor1@users.noreply.github.com>
2024-02-13 19:17:59 +0000

    25x

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
 
-poetry install
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-
-if [[ $CREATE_SUPERUSER ]];
-then
-  python bookingpiano/manage.py createsuperuser --no-input
-fi
+python manage.py createsuperuser --no-input
