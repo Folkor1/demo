22e2cb490652f8ea3507104f0c9bd377ee05ebfa
Folkor1 <98771910+Folkor1@users.noreply.github.com>
2024-02-13 15:40:18 +0000

    1x

--- bookingpiano/settings.py
+++ bookingpiano/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["demo-tomn.onrender.com", "localhost"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
